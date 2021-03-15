790082d867da03c396243188d803eae98e4e8880
VPA273 <vpa273@gmail.com>
2021-03-15 20:21:30 +0300

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brac = bracketsConfig.join('').replace(/,/g, '');
+        let stack = [];
+    for (let bracket of str) {
+            let brIndex = brac.indexOf(bracket)
+            if (brIndex % 2 === 0) {                
+                if (bracket === brac[brIndex + 1] && stack[stack.length - 1] === brIndex){
+                    stack.pop();
+                } else if (bracket === brac[brIndex + 1] && stack[stack.length - 1] !== brIndex) {
+                    stack.push(brIndex)
+                }
+                else{
+                    stack.push(brIndex)
+                }
+            } 
+            else {
+                if (stack.pop() !== brIndex-1){
+                    return false;
+                }
+            }            
+        }
+        return stack.length === 0
 }
